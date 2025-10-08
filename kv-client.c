1cc44df87a33d001f3ecf373ff4f052c0594203c
amandeep2102 <iamamandeep2002123@gmail.com>
2025-10-08 14:14:59 +0530

    INITIAL COMMIT

---
+++ kv-client.c
@@ -0,0 +1,239 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#define h_addr h_addr_list[0]
+
+char **tokens = NULL;
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+void tokenize(char *input)
+{
+    tokens = malloc(5 * sizeof(char *));
+
+    for (int i = 0; i < 5; i++)
+    {
+        tokens[i] = NULL;
+    }
+
+    const char *delim = " "; // space as delimiter
+    char *token = strtok(input, delim);
+    int i = 0;
+    while (token != NULL)
+    {
+        // printf("%s\n", token);
+        tokens[i] = malloc(strlen(token) + 1);
+        strcpy(tokens[i], token);
+        token = strtok(NULL, delim); // continue tokenizing
+        i++;
+    }
+}
+
+int send_string(int sockfd, const char *str)
+{
+    int len = strlen(str);
+    if (write(sockfd, &len, sizeof(int)) != sizeof(int))
+    {
+        perror("Error writing string length");
+        return -1;
+    }
+    if (write(sockfd, str, len) != len)
+    {
+        perror("Error writing string data");
+        return -1;
+    }
+    return 0; // success
+}
+
+int receive_string(int sockfd, char **out)
+{
+    int len;
+    if (read(sockfd, &len, sizeof(int)) != sizeof(int))
+    {
+        perror("Error reading string length");
+        return -1;
+    }
+    *out = malloc(len + 1);
+    if (!*out)
+        return -1;
+
+    if (read(sockfd, *out, len) != len)
+    {
+        perror("Error reading string data");
+        free(*out);
+        return -1;
+    }
+    (*out)[len] = '\0';
+    return 0; // success
+}
+
+int receivefunInt(int sockfd, int *var)
+{
+    int n;
+    n = read(sockfd, var, sizeof(int));
+    return n;
+}
+
+int sendfunInt(int sockfd, int *var)
+{
+    int n;
+    n = write(sockfd, var, sizeof(int));
+    return n;
+}
+
+int main(int argc, char *argv[])
+{
+    int alreadyconnected = 0;
+    char input[256];
+    int clisockfd, portno;
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+
+    while (1)
+    {
+        printf("> ");
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            printf("Error reading input\n");
+            return 1;
+        }
+
+        // Remove newline at the end if present
+        input[strcspn(input, "\n")] = '\0';
+
+        tokenize(input);
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     printf("token %d: %s\n", i, tokens[i]);
+        // }
+
+        if (strcmp(tokens[0], "connect") == 0)
+        {
+            if (alreadyconnected)
+            {
+                printf("Already Connected!!");
+                continue;
+            }
+
+            if (tokens[2] == NULL)
+            {
+                perror("Wrong Number of arguments");
+                continue;
+            }
+
+            portno = atoi(tokens[2]);
+            clisockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+            if (clisockfd == -1)
+            {
+                perror("Error opening socket");
+            }
+
+            // putting server info
+            server = gethostbyname(tokens[1]);
+            printf("Hostname is: %s\n", server->h_name);
+
+            if (server == NULL)
+            {
+                perror("Server Unreachable: ");
+            }
+
+            memset((char *)&serv_addr, 0, sizeof(serv_addr));
+            memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+            serv_addr.sin_port = htons(portno);
+            serv_addr.sin_family = AF_INET;
+
+            // connecting to the server
+
+            if (connect(clisockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+            {
+                perror("Error connecting to the server: ");
+            }
+
+            char *buffer;
+            receive_string(clisockfd, &buffer);
+            printf("%s \n", buffer);
+            alreadyconnected = 1;
+        }
+        else if (strcmp(tokens[0], "disconnect") == 0)
+        {
+            if (alreadyconnected)
+            {
+                close(clisockfd);
+                clisockfd = -1;
+                alreadyconnected = 0;
+                printf("Disconnected.\n");
+            }
+            else
+            {
+                printf("Not connected.\n");
+            }
+        }
+        else
+        {
+            int notokens = 0, n;
+            for (int i = 0; i < 5; i++)
+            {
+                if (tokens[i] == NULL)
+                {
+                    break;
+                }
+                notokens++;
+            }
+
+            sendfunInt(clisockfd, &notokens);
+            for (int i = 0; i < notokens; i++)
+            {
+                send_string(clisockfd, tokens[i]);
+            }
+
+            if (strcmp(tokens[0], "read") == 0)
+            {
+                char *value;
+                receive_string(clisockfd, &value);
+                // if (strcmp(value, "ERROR") == 0)
+                // {
+                //     printf("Key doesn't Exist!\n");
+                // }
+                // else
+                // {
+                //     printf("key -> %d, value -> %s \n", atoi(tokens[1]), value);
+                // }
+
+                printf("%s\n", value);
+            }
+            else if (strcmp(tokens[0], "update") == 0)
+            {
+                char *msg;
+                receive_string(clisockfd, &msg);
+                printf("%s\n", msg);
+            }
+            else if (strcmp(tokens[0], "delete") == 0)
+            {
+                char *msg;
+                receive_string(clisockfd, &msg);
+                printf("%s\n", msg);
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (tokens[i])
+                free(tokens[i]);
+        }
+        free(tokens);
+        tokens = NULL;
+    }
+
+    close(clisockfd);
+    return 0;
+}
---
+++ kv-server.c
@@ -0,0 +1,328 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include "uthash.h"
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+typedef struct
+{
+    int key;     // key
+    char *value; // value
+    UT_hash_handle hh;
+} kv_pair;
+
+kv_pair *table = NULL;
+
+int update_pair(int key, const int valuesize, const char *new_value)
+{
+    kv_pair *s;
+    HASH_FIND_INT(table, &key, s);
+    if (s)
+    {
+        free(s->value);
+        s->value = malloc(valuesize + 1);
+        strcpy(s->value, new_value);
+        s->value[valuesize] = '\0';
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void add_pair(const int *key, const int valuesize, const char *value)
+{
+    kv_pair *s = malloc(sizeof(kv_pair));
+    s->key = *key;
+    s->value = malloc(valuesize + 1);
+    strcpy(s->value, value);
+    s->value[valuesize] = '\0';
+    HASH_ADD_INT(table, key, s);
+}
+
+char *get_value(const int key)
+{
+    kv_pair *s;
+    HASH_FIND_INT(table, &key, s);
+    return s ? s->value : NULL;
+}
+
+int delete_pair(int key)
+{
+    kv_pair *s;
+    HASH_FIND_INT(table, &key, s);
+    if (s)
+    {
+        HASH_DEL(table, s);
+        free(s->value);
+        free(s);
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void delete_table()
+{
+    kv_pair *s, *tmp;
+    HASH_ITER(hh, table, s, tmp)
+    {
+        HASH_DEL(table, s);
+        free(s->value);
+        free(s);
+    }
+}
+
+char *concat_strings(int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+
+    int total_len = 0;
+    for (int i = 0; i < count; i++)
+    {
+        total_len += strlen(va_arg(args, const char *));
+    }
+
+    va_end(args);
+    char *result = malloc(total_len + 1);
+    if (!result)
+        return NULL;
+    result[0] = '\0';
+
+    va_start(args, count);
+    for (int i = 0; i < count; i++)
+    {
+        strcat(result, va_arg(args, const char *));
+    }
+    va_end(args);
+
+    return result;
+}
+
+int send_string(int sockfd, const char *str)
+{
+    int len = strlen(str);
+    if (write(sockfd, &len, sizeof(int)) != sizeof(int))
+    {
+        perror("Error writing string length");
+        return -1;
+    }
+    if (write(sockfd, str, len) != len)
+    {
+        perror("Error writing string data");
+        return -1;
+    }
+    return 0; // success
+}
+
+int receive_string(int sockfd, char **out)
+{
+    int len;
+    if (read(sockfd, &len, sizeof(int)) != sizeof(int))
+    {
+        perror("Error reading string length");
+        return -1;
+    }
+    *out = malloc(len + 1);
+    if (!*out)
+        return -1;
+
+    if (read(sockfd, *out, len) != len)
+    {
+        perror("Error reading string data");
+        free(*out);
+        return -1;
+    }
+    (*out)[len] = '\0';
+    return 0; // success
+}
+
+int receivefunInt(int sockfd, int *var)
+{
+    int n;
+    n = read(sockfd, var, sizeof(int));
+    return n;
+}
+
+void print_table()
+{
+    kv_pair *entry, *tmp;
+    printf("-------------PRINTING THE KEY VALUE PAIRS-----------------\n");
+    HASH_ITER(hh, table, entry, tmp)
+    {
+        printf("Key: %d, Value: %s\n", entry->key, entry->value);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in serv_addr, cli_addr;
+    int sockfd, portno, clilen, clisockfd;
+    char **tokens;
+
+    if (argc < 2)
+    {
+        error("Incorrect number of arguments: ");
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        error("Error opening Socket: ");
+    }
+
+    memset((char *)&serv_addr, 0, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        error("Error binding: ");
+    }
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    while (1)
+    {
+        clisockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (clisockfd < 0)
+        {
+            perror("Error Accepting the client connection: ");
+        }
+        send_string(clisockfd, "OK");
+
+        while (1)
+        {
+            int ntokens = 0;
+            tokens = malloc(5 * sizeof(char *));
+            for (int i = 0; i < 5; i++)
+            {
+                tokens[i] = NULL;
+            }
+
+            if (receivefunInt(clisockfd, &ntokens) <= 0)
+            {
+                printf("client is disconnected!\n");
+                break;
+            }
+
+            printf("ntokens %d\n", ntokens);
+
+            for (int i = 0; i < ntokens; i++)
+            {
+                if (receive_string(clisockfd, &tokens[i]) < 0)
+                {
+                    perror("error reading: ");
+                }
+            }
+
+            for (int i = 0; i < ntokens; i++)
+            {
+                printf("token %d %s \n", i, tokens[i]);
+            }
+
+            if (strcmp(tokens[0], "create") == 0)
+            {
+                if (ntokens < 4)
+                {
+                    perror("Invalid number of arguments!\n");
+                    continue;
+                }
+                int key = atoi(tokens[1]);
+                int valuesize = atoi(tokens[2]);
+                add_pair(&key, valuesize, tokens[3]);
+            }
+            else if (strcmp(tokens[0], "read") == 0)
+            {
+                if (ntokens < 2)
+                {
+                    perror("Invalid number of arguments!\n");
+                    continue;
+                }
+                int key = atoi(tokens[1]);
+                char *value = NULL;
+                value = get_value(key);
+
+                if (value == NULL)
+                {
+                    send_string(clisockfd, "ERROR KEY DOES NOT EXIST");
+                }
+                else
+                {
+                    char *msg;
+                    msg = concat_strings(4, "key -> ", tokens[1], " value -> ", value);
+                    printf("sending %s\n", msg);
+                    send_string(clisockfd, msg);
+                }
+            }
+            else if (strcmp(tokens[0], "update") == 0)
+            {
+                if (ntokens < 4)
+                {
+                    perror("Invalid number of arguments!\n");
+                    continue;
+                }
+                int key = atoi(tokens[1]);
+                int valuesize = atoi(tokens[2]);
+
+                if (update_pair(key, valuesize, tokens[3]) < 0)
+                {
+                    send_string(clisockfd, "ERROR KEY DOES NOT EXIST");
+                }
+                else
+                {
+                    send_string(clisockfd, "SUCCESS");
+                }
+            }
+            else if (strcmp(tokens[0], "delete") == 0)
+            {
+                int key = atoi(tokens[1]), out;
+                out = delete_pair(key);
+
+                if (out < 0)
+                {
+                    send_string(clisockfd, "ERROR KEY DOESN'T EXIST");
+                }
+                else
+                {
+                    send_string(clisockfd, "SUCCESS");
+                }
+            }
+        }
+        if (clisockfd != -1)
+        {
+            close(clisockfd);
+        }
+    }
+
+    int key = 5;
+    char *value = "anyuthoing";
+    add_pair(&key, strlen(value), value);
+
+    print_table();
+    close(sockfd);
+
+    if (clisockfd != -1)
+    {
+        close(clisockfd);
+    }
+
+    return 0;
+}
